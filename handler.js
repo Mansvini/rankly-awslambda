695b17bbaac7f4aff4cca51ea4536f03d7b2467f
Mansvini Aggarwal <89926191+Mansvini@users.noreply.github.com>
2022-04-01 15:13:45 +0530

    Add files via upload

---
+++ handler.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const emojis = [
+  '😄','😃','😀','😊','😉','😍','🔶','🔷', '🚀'
+];
+module.exports.rank = async (event) => {
+  const rank=event.queryStringParameters.rank;
+  const rankEmoji=emojis[rank >= emojis.length? emojis.length - 1 : rank];
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v3.0! Your function executed successfully!',
+        input: rankEmoji,
+      },
+      null,
+      2
+    ),
+  };
+};
